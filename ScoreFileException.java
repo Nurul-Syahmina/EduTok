683577f61e902efe2fafb0804a3c75317acc8c2c
TCO4 <bruceini2004@gmail.com>
2026-05-30 17:08:19 +0800

    Add files via upload

---
+++ ScoreFileException.java
@@ -0,0 +1,11 @@
+// Creator: Person 4 | Tester: Person 1
+// Group: G04/SE-G11 | App: EduTok
+
+package edutok.person4;
+
+public class ScoreFileException extends Exception {
+
+    public ScoreFileException(String message) {
+        super(message);
+    }
+}
